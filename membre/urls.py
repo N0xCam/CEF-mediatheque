7c8ab4270d4c1943bf33113ec2feebcade910a70
N0xCam <167630075+N0xCam@users.noreply.github.com>
2025-03-17 10:47:03 +0100

    path-ok

--- membre/urls.py
+++ membre/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('medias/', views.liste_medias, name='liste_medias_membre')
+    path('medias/', views.liste_membres, name='liste_medias_membre')
 ]
